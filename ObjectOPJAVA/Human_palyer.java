38d478f6d9a81bd8d74b586b8fd214b6179f1e90
HarperZhou529 <57165747+HarperZhou529@users.noreply.github.com>
2020-01-23 02:25:18 +0800

    新的提交
    更正的一次改了一个bug

---
+++ ObjectOPJAVA/Human_palyer.java
@@ -0,0 +1,76 @@
+package guessplayer;
+
+
+
+/**
+ * 
+ * @author zhouharper
+ *
+ */
+
+public class Human_palyer {
+	//静态常量：出拳的类型
+	public static final int messageTypeFirst=1;
+	//静态常量：出拳的信息
+	public static final int MessageTypeWin= 2;
+	public static final int MessageTypeLoss=3;
+	
+	
+	//静态常量：出拳类型的信息
+//属性：名称，分数，所出的拳，角色的台词
+	private String name;
+	private int score;
+	private int first;
+	//角色在出拳时说的话
+	private String[] firstwords={
+		"balalalalalalla",
+		"hahhhahhahahaha"
+	};
+	private String []winwords={
+		"hahhahahaha",
+		"kakakakkaak"
+	};
+	private String []losewords= {
+	"baabbababaabababab",
+	"jajajajajaajajajaj"
+	};
+	//方法：设置和得到名称，分数，说词，出拳
+		/** 对应humanplayer中定义的静态变量  @param msgType *///根据传入的消息类型，随机打印对应类型的消息
+	public void sendMessage(int msgType) {
+		//生成一个0-2之间的随机数（对应你有几句输出时的话语）
+		int index =(int)(Math.random()*2);
+		String message = null;
+		switch(msgType) {
+		case messageTypeFirst:
+		message=firstwords[index];
+		break;
+		case MessageTypeWin:
+		message=winwords[index];
+		break;
+		case MessageTypeLoss:
+		message=losewords[index];
+		break;
+		}
+		System.out.println(message);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public int getFirst() {
+		return first;
+	}
+	public void setFirst(int first) {
+		this.first = first;
+	}
+	
+	
+}
---
+++ ObjectOPJAVA/Testmain.java
@@ -0,0 +1,11 @@
+package guessplayer;
+
+public class Testmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+new GameRoom("admin","12345");
+
+	}
+
+}
